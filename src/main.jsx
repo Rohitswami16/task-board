8380e7e1397b8a7ab3992d00546a39343560d7ce
rohitswami16 <rohitswami1612@gmail.com>
2026-02-17 16:48:10 +0530

    deploye

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import App from "./App";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BoardProvider } from "./context/BoardContext";
+import { HashRouter } from "react-router-dom"; // ✅ import HashRouter
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BoardProvider>
-      <App />
+      <HashRouter> {/* ✅ Wrap App with HashRouter */}
+        <App />
+      </HashRouter>
     </BoardProvider>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  plugins: [react],
+  base: "/task-board/", // ✅ matches your GitHub repo name
+});
